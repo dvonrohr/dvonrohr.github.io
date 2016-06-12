e9ccb734238044cf532d9c8f46cc321a28caa099
Daniel Rudolf von Rohr <daniel@daniels-mbp.home>
2016-06-12 11:00:18 +0200

    Add basic off-canvas funtionality

---
+++ js/main.js
@@ -0,0 +1,17 @@
+(function() {
+
+  var slideout = new Slideout({
+    'panel': document.getElementById('panel'),
+    'menu': document.getElementById('menu'),
+    'side': 'right',
+    'padding': 256,
+    'tolerance': 70
+  });
+
+  // Toggle button
+  document.querySelector('.toggle-button').addEventListener('click', function() {
+    slideout.toggle();
+  });
+
+
+})();
