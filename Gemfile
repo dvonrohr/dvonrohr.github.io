3bfe8a8d67bbf5dc946cb4495c36c78b61679159
Daniel Rudolf von Rohr <d.rudolf.von.rohr@gmail.com>
2020-08-04 19:20:19 +0200

    [!!!] Switch to nuxtjs

--- Gemfile
+++
@@ -1,7 +0,0 @@
-source 'https://rubygems.org'
-
-gem 'rouge'
-gem 'jekyll-paginate'
-gem 'jekyll-watch'
-gem 'kramdown', :versions['kramdown']
-gem 'jekyll-sitemap'
---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="container">
+    <h1 class="text-xl">test</h1>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod ex facere
+      sequi dolorum optio aliquam vero molestiae laboriosam alias quo
+      consectetur perspiciatis a cum itaque, nesciunt accusantium molestias
+      corporis et?
+    </p>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
