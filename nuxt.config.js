d04d4c3414e22afee310fbc6dba2f0f03ff1d152
Daniel Rudolf von Rohr <d.rudolf.von.rohr@gmail.com>
2020-08-04 21:48:24 +0200

    feat: Change code block highlighting

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,37 +1,37 @@
-import path from 'path'
+import path from "path"
 
 export default {
   /*
    ** Nuxt rendering mode
    ** See https://nuxtjs.org/api/configuration-mode
    */
-  mode: 'universal',
+  mode: "universal",
   /*
    ** Nuxt target
    ** See https://nuxtjs.org/api/configuration-target
    */
-  target: 'static',
+  target: "static",
   /*
    ** Headers of the page
    ** See https://nuxtjs.org/api/configuration-head
    */
   head: {
-    title: process.env.npm_package_name || '',
+    title: process.env.npm_package_name || "",
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
       {
-        hid: 'description',
-        name: 'description',
-        content: process.env.npm_package_description || '',
+        hid: "description",
+        name: "description",
+        content: process.env.npm_package_description || "",
       },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
   },
   /*
    ** Global CSS
    */
-  css: ['~assets/styles/tailwind.css'],
+  css: ["~assets/styles/tailwind.css"],
   /*
    ** Plugins to load before mounting the App
    ** https://nuxtjs.org/guide/plugins
@@ -47,29 +47,35 @@ export default {
    */
   buildModules: [
     // Doc: https://github.com/nuxt-community/eslint-module
-    '@nuxtjs/eslint-module',
+    "@nuxtjs/eslint-module",
     // Doc: https://github.com/nuxt-community/nuxt-tailwindcss
-    '@nuxtjs/tailwindcss',
+    "@nuxtjs/tailwindcss",
   ],
   /*
    ** Nuxt.js modules
    */
   modules: [
-    '@nuxtjs/pwa',
+    "@nuxtjs/pwa",
     // Doc: https://github.com/nuxt/content
-    '@nuxt/content',
-    'nuxt-purgecss',
-    '@aceforth/nuxt-optimized-images',
+    "@nuxt/content",
+    "nuxt-purgecss",
+    "@aceforth/nuxt-optimized-images",
   ],
   purgeCSS: {
-    mode: 'postcss',
-    enabled: process.env.NODE_ENV === 'production',
+    mode: "postcss",
+    enabled: process.env.NODE_ENV === "production",
   },
   /*
    ** Content module configuration
    ** See https://content.nuxtjs.org/configuration
    */
-  content: {},
+  content: {
+    markdown: {
+      prism: {
+        theme: "prism-themes/themes/prism-material-oceanic.css",
+      },
+    },
+  },
   /*
    ** Build configuration
    ** See https://nuxtjs.org/api/configuration-build/
@@ -77,9 +83,9 @@ export default {
   build: {
     postcss: {
       plugins: {
-        'postcss-import': {},
-        tailwindcss: path.resolve(__dirname, './tailwind.config.js'),
-        'postcss-nested': {},
+        "postcss-import": {},
+        tailwindcss: path.resolve(__dirname, "./tailwind.config.js"),
+        "postcss-nested": {},
       },
     },
     preset: {
