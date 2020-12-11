5e0467cf3966d272ef9fdd8d49e524794c1bbb5f
Daniel Rudolf von Rohr <d.rudolf.von.rohr@gmail.com>
2020-12-12 00:08:59 +0100

    Prepare for gh-pages deployment

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,6 +11,7 @@ export default {
    ** See https://nuxtjs.org/api/configuration-target
    */
   target: "static",
+
   /*
    ** Headers of the page
    ** See https://nuxtjs.org/api/configuration-head
